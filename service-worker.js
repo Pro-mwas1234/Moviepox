f65d6fee87c878dc7136a93077bacd2522f001ea
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-01-24 10:14:16 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("equanta-cache").then(cache => {
+    caches.open("moviepox-cache").then(cache => {
       return cache.addAll(["/", "/index.html"]);
     })
   );
