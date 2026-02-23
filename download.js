eebee709b6f5873d52bf64e787117427ce944e50
Pro-mwas1234 <130271674+Pro-mwas1234@users.noreply.github.com>
2026-02-23 14:19:12 +0300

    Update download.js

--- download.js
+++ download.js
@@ -42,8 +42,8 @@ class DownloadManager {
 
         // Build clean Vidsrc URL (NO extra spaces!)
         const url = mediaType === 'tv'
-            ? `https://dl.vidsrc.vip/tv/${itemId}/${season}/${episode}`
-            : `https://dl.vidsrc.vip/movie/${itemId}`;
+            ? `https://02moviedownloader.site/api/download/tv/${itemId}/${season}/${episode}`
+            : `https://02moviedownloader.site/api/download/movie/${itemId}`;
 
         // ➤ GOOGLE ANALYTICS: Track download event
         if (typeof gtag === 'function') {
